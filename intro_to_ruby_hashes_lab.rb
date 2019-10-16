58c9d1cabf57bcb4dae8d00f5df7c522786591ad
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-16 01:03:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  if my_hash_creator {key: value}
+  if my_hash_creator [key:]
     puts my_hash_creator{key: value}
   end
 end
